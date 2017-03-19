25c9e293598cfdcb98e647b13b44c0cadf901793
Victor Follet <follet.victor@gmail.com>
2017-03-19 03:32:45 +0300

    stackoverflow with idea for test (Idea for test: scored RDD should have 2121822 entries)

---
+++ stackoverflow/src/test/scala/stackoverflow/StackOverflowSuite.scala
@@ -0,0 +1,50 @@
+package stackoverflow
+
+import org.scalatest.{FunSuite, BeforeAndAfterAll}
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.rdd.RDD
+import java.net.URL
+import java.nio.channels.Channels
+import java.io.File
+import java.io.FileOutputStream
+
+@RunWith(classOf[JUnitRunner])
+class StackOverflowSuite extends FunSuite with BeforeAndAfterAll {
+  System.setProperty("hadoop.home.dir", "D:\\Users\\DarthVictor\\Documents\\Scala\\scala-spark-big-data")
+  lazy val testObject = new StackOverflow {
+    override val langs =
+      List(
+        "JavaScript", "Java", "PHP", "Python", "C#", "C++", "Ruby", "CSS",
+        "Objective-C", "Perl", "Scala", "Haskell", "MATLAB", "Clojure", "Groovy")
+    override def langSpread = 50000
+    override def kmeansKernels = 45
+    override def kmeansEta: Double = 20.0D
+    override def kmeansMaxIterations = 120
+  }
+
+  test("testObject can be instantiated") {
+    val instantiatable = try {
+      testObject
+      true
+    } catch {
+      case _: Throwable => false
+    }
+    assert(instantiatable, "Can't instantiate a StackOverflow object")
+  }
+
+
+//  test("scored RDD should have 2121822 entries") {
+//    @transient lazy val conf: SparkConf = new SparkConf().setMaster("local").setAppName("StackOverflow")
+//    @transient lazy val sc: SparkContext = new SparkContext(conf)
+//    val lines   = sc.textFile("src/main/resources/stackoverflow/stackoverflow.csv")
+//    val raw     = testObject.rawPostings(lines)
+//    val grouped = testObject.groupedPostings(raw)
+//    val scored  = testObject.scoredPostings(grouped)
+//    assert(scored.count() == 2121822, "Wrong number of entries")
+//  }
+//
+}
